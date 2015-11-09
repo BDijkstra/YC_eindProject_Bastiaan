c382b168b7bdf2968208f271384d7b7439ba8d9b
Kevin <kevinrijs@gmail.com>
2015-11-09 15:52:08 +0100

    no message

--- src/nl/zwolle/zeeslag/Speler.java
+++ src/nl/zwolle/zeeslag/Speler.java
@@ -3,36 +3,37 @@ package nl.zwolle.zeeslag;
 public class Speler {
 
 	private String naam;
-	
-	
+
+
 	public Speler(){
 		this("AI");
-		
+
 	}
-	
+
 	public Speler(String naam){
 		this.naam = naam;
 	}
-	
+
 	public void schietOpVakje(Bord b, int x, int y){
-	
-		
+
+
 		if(b.checkGeldigheidCoordinaten(x, y)){
-			
-		b.vakjeArray[x][y].setBeschoten(true);
-		
-		
-		if (b.vakjeArray[x][y].isBevatBoot()){
-			System.out.println("Boem!");
-		} else{
-			System.out.println("Plons");
+
+			b.vakjeArray[x][y].setBeschoten(true);
+
+
+			if (b.vakjeArray[x][y].isBevatBoot()){
+				System.out.println("Boem!");
+			} else{
+				System.out.println("Plons");
+			}
+
+
+
+
+
 		}
-		
-		
-		
-		
-		
+
+
 	}
-	
-	
 }
