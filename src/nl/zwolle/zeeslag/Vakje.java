3d97761847ff7725ea3a818f0233f9ee8a776a18
Bastiaan <bastiaandijkstra@gmail.com>
2015-11-09 14:34:33 +0100

    update aan de Vakje class

--- src/nl/zwolle/zeeslag/Vakje.java
+++ src/nl/zwolle/zeeslag/Vakje.java
@@ -1,10 +1,38 @@
 package nl.zwolle.zeeslag;
 
 public class Vakje {
-private boolean bevatBoot;
-private boolean beschoten;
+	private boolean bevatBoot;
+	private boolean beschoten;
 
-public void checkOfBootNogLeeft(){
-	
-}
+	public boolean isBevatBoot() {
+		return bevatBoot;
+	}
+
+	public void setBevatBoot(boolean bevatBoot) {
+		this.bevatBoot = bevatBoot;
+	}
+
+	public boolean isBeschoten() {
+		return beschoten;
+	}
+
+	public void setBeschoten(boolean beschoten) {
+		this.beschoten = beschoten;
+	}
+
+	public boolean checkOfBootNogLeeft() {
+
+		// haalt de lengte van het bootobject op en trekt daar 1 van af. Als de
+		// lengte van de boot 0 is, word deze op 'dood' gezet.
+		if (bevatBoot) {
+			if (beschoten) {
+				// if(boot.getLengte()==0){
+				// int veranderLengte =boot.getLengte() -1;
+				// boot.setLengte(veranderLengte);
+				return false;
+			}
+		}
+
+		return true;
+	}
 }
